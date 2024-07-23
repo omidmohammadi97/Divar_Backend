1e2c974d1d4069da98ca54ce3d5519eafb04f548
omidmohammadi97 <om.pelarti@gmail.com>
2024-07-23 18:08:03 +0330

    config multer for uploading pictures

--- src/common/utils/multer.js
+++ src/common/utils/multer.js
@@ -1,5 +1,30 @@
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer  = require('multer');
+const fs = require("fs");
+const path = require("path");
+const { HttpStatusCode } = require('axios');
+const storage = multer.diskStorage({
+    destination : function(req , file ,cb){ 
+        fs.mkdirSync(path.join(process.cwd() , "public" , "public") , {recursive : true});
+        cb(null , "public/upload")
+    },
+    filename : function(req , file , cb){
+        
+        const whiteList = ["image/png" , "image/jpg" , "image/jpeg" , "image/webp"];
+        if(whiteList.includes(file.mimetype)){
+            let format = path.extname(file.orginalname);
+            let fileName = new Date().getTime().toString() + format;
+            cb(null , fileName);
+        }else{
+            cb(new HttpStatusCode.BadRequest("format of pictures are not allowed!"))
+        }
+    } 
+})
+const upload = multer({
+    storage,
+    limits : {
+        fileSize : 3 * 1000 * 1000
+    }
+});
 
 module.exports = {
     upload
